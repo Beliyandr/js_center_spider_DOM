512fd5c6ba39f0e02f16d283f137ec16e8548e4f
beliyandr <beliyandr@gmail.com>
2024-06-30 12:23:31 +0300

    make build

---
+++ index.fb81a23d.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=e.querySelector(".spider");t.style.top=e.clientHeight/2+"px",t.style.left=e.clientWidth/2+"px",t.style.transform="translate(-50%,-50%)";
+//# sourceMappingURL=index.fb81a23d.js.map
